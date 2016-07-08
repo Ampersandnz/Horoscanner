19a08c1b7d1f34eaaf966ca68d6e02deb88f8773
Michael <nz.ampersand@gmail.com>
2016-07-08 15:28:37 +1200

    Added example code from a tutorial on the Android native barcode scanning API

--- app/src/main/java/com/android/michaello/horoscanner/MainActivity.java
+++ app/src/main/java/com/android/michaello/horoscanner/MainActivity.java
@@ -1,15 +1,22 @@
 package com.android.michaello.horoscanner;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.SparseArray;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +35,14 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                processImage();
+            }
+        });
     }
 
     /**
@@ -41,25 +56,39 @@ public class MainActivity extends AppCompatActivity {
         textField.setText(message);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void testFunction(View view) {
+        //TODO: This function opens a camera activity
+        //Intent intent = new Intent(this, );
+
+        //startActivityForResult(intent, );
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private void processImage() {
+        ImageView myImageView = (ImageView) findViewById(R.id.imgview);
+        Bitmap myBitmap = BitmapFactory.decodeResource(
+                getApplicationContext().getResources(),
+                R.drawable.puppy);
+        myImageView.setImageBitmap(myBitmap);
+        TextView txtView = (TextView) findViewById(R.id.txtContent);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        BarcodeDetector detector =
+                new BarcodeDetector.Builder(getApplicationContext())
+                        .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
+                        .build();
+
+        if (!detector.isOperational()) {
+            txtView.setText(R.string.error_no_detector);
+            return;
         }
 
-        return super.onOptionsItemSelected(item);
+        Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
+        SparseArray<Barcode> barcodes = detector.detect(frame);
+
+        Barcode thisCode = barcodes.valueAt(0);
+        txtView.setText(thisCode.rawValue);
     }
+
+    //TODO: Implement the onActivityReturn function callback thing to process the returned value
+    // in this case, we'll just display it somewhere on the main screen but later it'll be
+    // actually used to generate data to show the user.
 }
