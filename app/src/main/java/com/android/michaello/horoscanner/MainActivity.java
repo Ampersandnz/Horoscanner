4dccea8264a59f4a46c6b9690fcb11e8b6d303fd
Michael Lo <nz.ampersand@gmail.com>
2016-07-10 18:01:57 +1200

    Reworked UI (Still just trying stuff out and finding how I want to structure my architecture), moved scanning work out into subclasses of an interface. Also added a bunch of features/components to the TODO list.

--- app/src/main/java/com/android/michaello/horoscanner/MainActivity.java
+++ app/src/main/java/com/android/michaello/horoscanner/MainActivity.java
@@ -1,24 +1,16 @@
 package com.android.michaello.horoscanner;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.SparseArray;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.android.gms.vision.Frame;
-import com.google.android.gms.vision.barcode.Barcode;
-import com.google.android.gms.vision.barcode.BarcodeDetector;
-
 public class MainActivity extends AppCompatActivity {
+    private Scanner scanner;
+    //private ScanResult lastScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,60 +27,29 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-
-        Button btn = (Button) findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                processImage();
-            }
-        });
     }
 
-    /**
-     * Called when the user clicks the View button
-     */
-    public void setText(View view) {
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        TextView textField = (TextView) findViewById(R.id.textField);
+    public void scan(View v) {
+        TextView txtView = (TextView) findViewById(R.id.txt_scan_result);
 
-        String message = editText.getText().toString();
-        textField.setText(message);
-    }
+        switch (v.getId()) {
+            case R.id.btn_scan_test_text:
+                this.scanner = new TestScanner(getApplicationContext());
+                break;
 
-    public void testFunction(View view) {
-        //TODO: This function opens a camera activity
-        //Intent intent = new Intent(this, );
+            case R.id.btn_scan_test_image:
+                this.scanner = new TestImageScanner(getApplicationContext());
+                break;
 
-        //startActivityForResult(intent, );
-    }
-
-    private void processImage() {
-        ImageView myImageView = (ImageView) findViewById(R.id.imgview);
-        Bitmap myBitmap = BitmapFactory.decodeResource(
-                getApplicationContext().getResources(),
-                R.drawable.puppy);
-        myImageView.setImageBitmap(myBitmap);
-        TextView txtView = (TextView) findViewById(R.id.txtContent);
-
-        BarcodeDetector detector =
-                new BarcodeDetector.Builder(getApplicationContext())
-                        .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
-                        .build();
-
-        if (!detector.isOperational()) {
-            txtView.setText(R.string.error_no_detector);
-            return;
+            default:
+                this.scanner = null;
+                txtView.setText(R.string.error_no_scanner);
+                return;
         }
 
-        Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
-        SparseArray<Barcode> barcodes = detector.detect(frame);
+        String result = scanner.scan();
 
-        Barcode thisCode = barcodes.valueAt(0);
-        txtView.setText(thisCode.rawValue);
+        //TODO: Convert to calling getString(), getImage(), getNumber() on the ScanResult object returned and, if they're not null, set the corresponding views.
+        txtView.setText(result);
     }
-
-    //TODO: Implement the onActivityReturn function callback thing to process the returned value
-    // in this case, we'll just display it somewhere on the main screen but later it'll be
-    // actually used to generate data to show the user.
 }
